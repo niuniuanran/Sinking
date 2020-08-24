e1b1af5ff256eff189dcbee4975047d19dbb1b20
niuniuanran <irene.anran.niu@gmail.com>
2020-08-24 21:53:56 +1200

    Add people table into body panel

--- src/components/body/Body.jsx
+++ src/components/body/Body.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import style from './body.module.css'
+import PeopleTable from "../table/PeopleTable";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 export default function Body({currentPanel}) {
-    return <div className={style.body}>
+    const [people, setPeople] = useState([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        fetch("https://public.opendatasoft.com/api/records/1.0/search/?dataset=titanic-passengers&q=&rows=1000")
+            .then(res => res.json())
+            .then(json => {
+                setPeople(json.records);
+                console.log(json.records);
+                setLoading(false)
+            });
+    }, []);
 
+    return <div className={style.body}>
+        {loading && <CircularProgress className={style.loading}/>}
+        {!loading && currentPanel === 'people' && <PeopleTable peopleRecord={people}/>}
+        {!loading && currentPanel === 'stats' && <div>Stats</div>}
 
     </div>
 }
--- src/components/table/PeopleTable.jsx
+++ src/components/table/PeopleTable.jsx
@@ -5,29 +5,25 @@ import {
     TableRow,
     TableBody,
     TableHead,
-
-    Card, CardContent, Typography
 } from "@material-ui/core";
 
 export default function PeopleTable({peopleRecord}) {
     const tableHeadings = peopleRecord && peopleRecord[0] && Object.keys(peopleRecord[0].fields);
     console.log(tableHeadings);
-    return <Card>
-        <CardContent>
-            {tableHeadings && <Table>
-                <TableHead>
-                    <TableRow>
-                        {tableHeadings.map((key, i) => <TableCell key={`heading_${i}`}>{key}</TableCell>)}
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {
-                        peopleRecord.map((data) => <TableRow key={data.recordid}>
-                            {Object.values(data.fields).map((d, i) => <TableCell key={`cell_${i}`}>{d}</TableCell>)}
-                        </TableRow>)
-                    }
-                </TableBody>
-            </Table>}
-        </CardContent>
-    </Card>
+    return <>
+        {tableHeadings && <Table>
+            <TableHead>
+                <TableRow>
+                    {tableHeadings.map((key, i) => <TableCell key={`heading_${i}`}>{key}</TableCell>)}
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {
+                    peopleRecord.map((data) => <TableRow key={data.recordid}>
+                        {Object.values(data.fields).map((d, i) => <TableCell key={`cell_${i}`}>{d}</TableCell>)}
+                    </TableRow>)
+                }
+            </TableBody>
+        </Table>}
+    </>
 }
