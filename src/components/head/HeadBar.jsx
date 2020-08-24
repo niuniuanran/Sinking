600e578bdff53ef26cfe11c6ef890f34c7ca0ce8
niuniuanran <irene.anran.niu@gmail.com>
2020-08-24 20:21:47 +1200

    Add the people and chart buttons

--- src/components/head/HeadBar.jsx
+++ src/components/head/HeadBar.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 import style from './head.module.css'
+import Grid from "@material-ui/core/Grid";
+import EqualizerRoundedIcon from '@material-ui/icons/EqualizerRounded';
+import PeopleIcon from '@material-ui/icons/People';
+import IconButton from "@material-ui/core/IconButton";
 
 export default function HeadBar() {
     return <div>
         <div className={style.headBar}>
-            <span className={style.title}>Sinking</span><span style={style.subtitle}>the Titanic story</span>
+            <Grid container>
+                <Grid className={style.titles} item xs={12} sm={6}>
+                    <span className={style.title}>Sinking</span>
+                    <span className={style.subtitle}>the Titanic story</span>
+                </Grid>
+                <Grid item xs={12} sm={6} container spacing={5} className={style.buttons}>
+                    <Grid item xs={6}>
+                        <IconButton size={"medium"} className={`${style.headButton} ${style.peopleButton}`}>
+                            <PeopleIcon/>
+                        </IconButton>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <IconButton size={'medium'} className={style.headButton}>
+                            <EqualizerRoundedIcon/>
+                        </IconButton>
+                    </Grid>
+                </Grid>
+            </Grid>
         </div>
         <WaveEffect/>
     </div>
